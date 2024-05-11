0985ef8926a8e7f501a9a09a3eb9ec9a5b04b4e6
Frederick Butterfly <keitumetsemogotsif@gmail.com>
2024-05-11 17:16:46 +0200

    Upload modal and functionality

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,10 +1,23 @@
 "use client";
 
+import useAuthModal from "@/hooks/useAuthModal";
+import useUploadModal from "@/hooks/useUploadModal";
+import { useUser } from "@/hooks/useUser";
 import { AiOutlinePlus } from "react-icons/ai";
 import { TbPlaylist } from "react-icons/tb";
 
 const Library = () => {
-  const onClick = () => {};
+  const autoModal = useAuthModal()
+  const { user } = useUser()
+  const uploadModal = useUploadModal()
+
+  const onClick = () => {
+    if (!user) {
+      return autoModal.onOpen()
+    }
+
+    return uploadModal.onOpen()
+  };
 
   return (
     <div className="flex flex-col">
--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import AuthModal from "@/components/AuthModal";
+import UploadModal from "@/components/UploadModal";
 import { useEffect, useState } from "react";
 
 const ModalProvider = () => {
@@ -14,7 +15,12 @@ const ModalProvider = () => {
     return null;
   }
 
-  return <AuthModal />;
+  return (
+    <>
+      <AuthModal />
+      <UploadModal />
+    </>
+  )
 };
 
 export default ModalProvider;
