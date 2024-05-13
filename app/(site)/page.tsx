4298b0176e0eff475564472d86326d14b52bff0e
Frederick Butterfly <keitumetsemogotsif@gmail.com>
2024-05-13 23:43:57 +0200

    Favourite Functionality

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -16,7 +16,7 @@ export default async function Home() {
           </h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-4 gap-3 mt-4">
             <ListItem 
-              image="/images/liked.png" name="Liked Songs" href="Liked"/>
+              image="/images/liked.png" name="Liked Songs" href="liked"/>
           </div>
         </div>
       </Header>
